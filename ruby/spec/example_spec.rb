b6eac32d74a4e6571182db396aab81a055ebc813
Alex Aitken <alex.aitken@shopify.com>
2019-07-26 19:20:51 -0400

    Add some kind of test to get some coverage.

--- ruby/spec/example_spec.rb
+++ ruby/spec/example_spec.rb
@@ -1,7 +1,78 @@
 require 'spec_helper'
+require 'ugly_trivia/game'
 
-describe "example test" do
-  it "should pass" do
-    true.should == true
+describe "overall game" do
+  it "play a bit of a game" do
+
+    game = UglyTrivia::Game.new
+
+    game.add('alex')
+    game.add('chunky')
+
+    game.roll(1)
+    expect(game.was_correctly_answered).to eq(true)
+    game.roll(2)
+    expect(game.was_correctly_answered).to eq(true)
+
+    game.roll(1)
+    expect(game.was_correctly_answered).to eq(true)
+    game.roll(2)
+    expect(game.was_correctly_answered).to eq(true)
+
+    game.roll(1)
+    expect(game.was_correctly_answered).to eq(true)
+    game.roll(2)
+    expect(game.was_correctly_answered).to eq(true)
+
+    game.roll(1)
+    expect(game.was_correctly_answered).to eq(true)
+    game.roll(2)
+    expect(game.was_correctly_answered).to eq(true)
+
+    game.roll(1)
+    expect(game.was_correctly_answered).to eq(true)
+    game.roll(2)
+    expect(game.was_correctly_answered).to eq(true)
+
+    game.roll(1)
+    expect(game.was_correctly_answered).to eq(false)
+  end
+
+  it "play with 1 player in box alot bit of a game" do
+
+    game = UglyTrivia::Game.new
+
+    game.add('alex')
+    game.add('chunky')
+
+    game.roll(1)
+    expect(game.wrong_answer).to eq(true)
+    game.roll(2)
+    expect(game.was_correctly_answered).to eq(true)
+
+    game.roll(1)
+    expect(game.was_correctly_answered).to eq(true)
+    game.roll(2)
+    expect(game.was_correctly_answered).to eq(true)
+
+    game.roll(1)
+    expect(game.was_correctly_answered).to eq(true)
+    game.roll(2)
+    expect(game.was_correctly_answered).to eq(true)
+
+    game.roll(1)
+    expect(game.was_correctly_answered).to eq(true)
+    game.roll(2)
+    expect(game.was_correctly_answered).to eq(true)
+
+    game.roll(1)
+    expect(game.was_correctly_answered).to eq(true)
+    game.roll(2)
+    expect(game.was_correctly_answered).to eq(true)
+
+    game.roll(1)
+    expect(game.was_correctly_answered).to eq(true)
+    game.roll(2)
+    expect(game.was_correctly_answered).to eq(false)
   end
 end
