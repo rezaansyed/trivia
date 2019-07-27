b0b4a58bd99a8c006c0905ab98419a73e92c9588
Alex Aitken <alex.aitken@shopify.com>
2019-07-26 20:24:47 -0400

    Use odd? instead of modding

--- ruby/lib/ugly_trivia/game.rb
+++ ruby/lib/ugly_trivia/game.rb
@@ -53,7 +53,7 @@ module UglyTrivia
       @output.write "They have rolled a #{roll}"
 
       if @in_penalty_box[@current_player]
-        if roll % 2 != 0
+        if roll.odd?
           @is_getting_out_of_penalty_box = true
 
           @output.write "#{@players[@current_player]} is getting out of the penalty box"
